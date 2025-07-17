850250f9ccc54ea9ae3a7f0bc9b80496b7f57896
sreejith <aimanamsreejith19@gmail.com>
2025-07-17 21:27:25 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine','hbs')
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post('/formsubmisson',(req,res)=>{
+ // console.log(req)
+  const height=req.body.height
+  const weight=req.body.weight
+  const age=req.body.age 
+  let height_in_metr =height/100
+  let bmi=weight/(height_in_metr*height_in_metr)
+  bmi = parseFloat(bmi.toFixed(2));
+  let data={height,weight,age,bmi}
+  // res.send(`${height} weight is ${weight} and age is ${age}`) 
+  res.render('submit',data)
+})
+
+
+
+
+
+
+function print() {
+  console.log("server started at", port);
+}
+app.listen(port, "0.0.0.0", print);
+
+
